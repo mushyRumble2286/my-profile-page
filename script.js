f5677c3e34ff277369b80a638da6b68970525ba5
Favour <mushyon2k@gmail.com>
2024-07-24 10:08:23 +0100

    script update

---
+++ script.js
@@ -0,0 +1,16 @@
+function lgncheck(){
+    const userId = document.getElementById("userid").value;
+    const password = document.getElementById("password").value;
+    var correctUsername = "Aptech";
+    var correctPassword = "Aptech1111";
+    if (userId === "" , password === "") {
+        alert("Fields cannot be empty");
+    } 
+    else if (userId == correctUsername , password == correctPassword) {
+        alert("Login successful");
+        window.location.href = "dashboard.html";
+    } 
+    else {
+        alert("Wrong username or password");
+    };
+}
